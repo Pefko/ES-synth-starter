37fb4ac4c21f2c06fc11a900059495653df1da9d
Nicholas Mytilineos <nicholas.mytilineos@hotmail.com>
2023-03-03 18:05:17 +0000

    working volume control

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
   const uint32_t interval = 100; //Display update interval
   volatile uint32_t currentStepSize;
   volatile uint8_t keyArray[7];
+  volatile uint8_t knob3;
+  volatile uint8_t rotation = 0;
   const char * globalKeySymbol;
   SemaphoreHandle_t keyArrayMutex;
 
@@ -102,10 +104,43 @@ uint8_t readCols(){
 
 }
 
+uint8_t readKnobCols(){
+  // digitalWrite(REN_PIN, HIGH);
+  // digitalWrite(RA0_PIN, LOW);
+  // digitalWrite(RA1_PIN, LOW);
+  // digitalWrite(RA2_PIN, LOW);
+
+  uint8_t c0 = digitalRead(C0_PIN);
+  uint8_t c1 = digitalRead(C1_PIN);
+  uint8_t c2 = digitalRead(C2_PIN);
+  uint8_t c3 = digitalRead(C3_PIN);
+
+  // uint8_t col = digitalRead(C0_PIN) ? 0 : (digitalRead(C1_PIN) ? 1 : (digitalRead(C2_PIN) ? 2 : (digitalRead(C3_PIN) ? 3 : 100)));
+
+  uint8_t result1 = 0;
+  uint8_t result2 = 0;
+  result1 |= (c0 << 0);
+  result1 |= (c1 << 1);
+  // result2 |= (c2 << 2);
+  // result2 |= (c3 << 3);
+
+  // Serial.println(result1);
+
+  //uint8_t result = (c3 << 3) | (c2 << 2) | (c1 << 1) | c0;
+  return result1;
+
+}
+
 void sampleISR() {
   static uint32_t phaseAcc = 0;
   phaseAcc += currentStepSize;
   int32_t Vout = (phaseAcc >> 24) - 128;
+  if(rotation==0){
+    Vout=0;
+  }
+  else{
+    Vout = Vout >> (8 - rotation);
+  }
   analogWrite(OUTR_PIN, Vout + 128);
   // delayMicroseconds(2000);
   // analogWrite(OUTR_PIN, 0);
@@ -113,7 +148,7 @@ void sampleISR() {
 
 void scanKeysTask(void * pvParameters) {
 
-  const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
+  const TickType_t xFrequency = 20/portTICK_PERIOD_MS;
   TickType_t xLastWakeTime = xTaskGetTickCount();
 
   while(1){
@@ -121,17 +156,30 @@ void scanKeysTask(void * pvParameters) {
 
     const uint8_t cols[] = {C0_PIN, C1_PIN, C2_PIN, C3_PIN};
     uint32_t lastStepSize = 0;
+    uint8_t result1;
+    uint8_t temprotate3 = rotation;
+    bool turnRight;
     char * keysymbol = 0;
+    // uint8_t key1, key2;
+    uint8_t knob3before = knob3;
     const uint32_t * localStepSizes = stepSizes;
     
+    //this is meant to wrap any keyArray accesses, 
+    //maybe wrapping too much uncessary code rn (therefore locking for too long causing problems potentially)
     xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
-    for(uint8_t row = 0; row < 3; row++){
+    for(uint8_t row = 0; row < 4; row++){
       setRow(row);
       delayMicroseconds(10);
-      uint8_t keys = readCols();
-      
-      keyArray[row] = keys;
-      
+      if(row!=3){
+        uint8_t keys = readCols();
+        keyArray[row] = keys;
+      }
+      else if(row==3){
+        result1 = readKnobCols();
+        // Serial.println(result1);
+        // Serial.println(knob3);
+        // Serial.println("-------------------------");
+      }
     }
 
     for(uint8_t i=0;i<3;i++){
@@ -155,12 +203,36 @@ void scanKeysTask(void * pvParameters) {
         lastStepSize = lastStepSize;
         keysymbol = keysymbol;
       }
+
       xSemaphoreGive(keyArrayMutex);
       
       // currentStepSize = lastStepSize;
       __atomic_store_n(&currentStepSize, lastStepSize, __ATOMIC_RELAXED);
       __atomic_store_n(&globalKeySymbol, keysymbol, __ATOMIC_RELAXED);
     }
+    //add knob decoding here:
+    if((knob3==0 && result1==1) || (knob3==3 && result1==2)){
+      if(temprotate3 < 8){
+        temprotate3 +=1;
+      }
+      turnRight = true;
+    }
+    else if((knob3==1 && result1==0) || (knob3==2 && result1==3)){
+      if(temprotate3 > 0){
+        temprotate3 -=1;
+      }
+      turnRight = false;
+    }
+    else if((knob3==0 && result1==3) || (knob3==1 && result1==2) || (knob3==2 && result1==1) || (knob3==3 && result1==0)){
+      if(turnRight && temprotate3 < 8){
+        temprotate3 +=1;
+      }
+      else if(!turnRight && temprotate3 > 0){
+        temprotate3 -=1;
+      }
+    }
+    __atomic_store_n(&rotation, temprotate3, __ATOMIC_RELAXED);
+    knob3 = result1;
   }
 }
 
@@ -185,6 +257,8 @@ void displayUpdateTask(void * pvParameters){
     
     // Store the result in the global variable
     u8g2.drawStr(2,30,globalKeySymbol);
+    u8g2.setCursor(30,30);
+    u8g2.print(rotation);
     u8g2.sendBuffer();          // transfer internal memory to the display
 
     //Toggle LED
@@ -233,21 +307,21 @@ void setup() {
 
   TaskHandle_t scanKeysHandle = NULL;
   xTaskCreate(
-    scanKeysTask,		/* Function that implements the task */
-    "scanKeys",		  /* Text name for the task */
-    64,      		    /* Stack size in words, not bytes */
-    NULL,			      /* Parameter passed into the task */
-    2,			        /* Task priority */
-    &scanKeysHandle /* Pointer to store the task handle */
+    scanKeysTask,		  /* Function that implements the task */
+    "scanKeys",		    /* Text name for the task */
+    64,      		      /* Stack size in words, not bytes */
+    NULL,			        /* Parameter passed into the task */
+    2,			          /* Task priority */
+    &scanKeysHandle   /* Pointer to store the task handle */
   );	
 
   TaskHandle_t displayUpdateHandle = NULL;
   xTaskCreate(
-    displayUpdateTask,		/* Function that implements the task */
-    "displayUpdate",		  /* Text name for the task */
-    64,      		          /* Stack size in words, not bytes */
-    NULL,			            /* Parameter passed into the task */
-    1,			              /* Task priority */
+    displayUpdateTask,		  /* Function that implements the task */
+    "displayUpdate",		    /* Text name for the task */
+    64,      		            /* Stack size in words, not bytes */
+    NULL,			              /* Parameter passed into the task */
+    1,			                /* Task priority */
     &displayUpdateHandle    /* Pointer to store the task handle */
   );	
 
